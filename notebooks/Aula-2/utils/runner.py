c68fed934305fc0857d53f7e0330cd2683176797
Thiago P. Bueno <thiago.pbueno@gmail.com>
2020-02-08 10:59:55 -0300

    feat(aula2): add utils.runner.evaluate and utils.runner.train
    Signed-off-by: Thiago P. Bueno <thiago.pbueno@gmail.com>

--- notebooks/Aula-2/utils/runner.py
+++ notebooks/Aula-2/utils/runner.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 
 
-def run(agent, env, total_timesteps):
+def train(agent, env, total_timesteps):
     timesteps = []
     total_rewards = []
     avg_total_rewards = []
@@ -43,5 +43,50 @@ def run(agent, env, total_timesteps):
             ratio = int(100 * timestep / total_timesteps)
             uptime = int(time.time() - start_time)
             print(f"[{ratio:3d}% / {uptime:3d}s] timestep = {timestep}/{total_timesteps}, episode = {episode:3d} -> loss = {loss:10.4f}, total_reward = {total_reward:10.4f}, episode_length = {episode_length:3d}\r", end="")
+
+    return timesteps, total_rewards, avg_total_rewards
+
+
+def evaluate(agent, env, n_episodes, render=False):
+    timesteps = []
+    total_rewards = []
+    avg_total_rewards = []
+
+    timestep = 0
+    episode = 0
+
+    start_time = time.time()
+
+    for episode in range(1, n_episodes + 1):
+        total_reward = 0.0
+        episode_length = 0
+
+        obs = env.reset()
+        done = False
+
+        while not done:
+            action = agent.act(obs)
+            next_obs, reward, done, _  = env.step(action)
+            agent.observe(obs, action, reward, next_obs, done)
+
+            total_reward += reward
+            episode_length += 1
+            timestep += 1
+
+            obs = next_obs
+
+            if render:
+                env.render(mode="human")
+
+        if render:
+            env.close()
+
+        timesteps.append(timestep)
+        total_rewards.append(total_reward)
+        avg_total_rewards.append(np.mean(total_rewards[-100:]))
+
+        ratio = int(100 * episode / n_episodes)
+        uptime = int(time.time() - start_time)
+        print(f"[{ratio:3d}% / {uptime:3d}s] episode = {episode}/{n_episodes} -> total_reward = {total_reward:10.4f}, episode_length = {episode_length:3d}\r", end="")
 
     return timesteps, total_rewards, avg_total_rewards
